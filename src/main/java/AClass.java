cc6b0dccc3fac7ea4dd6fcf9638c78e51c3caec3
Lucas <lucasdornellesr@gmail.com>
2019-11-05 19:53:54 -0300

    initial commit

---
+++ src/main/java/AClass.java
@@ -0,0 +1,16 @@
+public class AClass {
+
+    private String testString;
+
+    public AClass(String testString) {
+        this.testString = testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }
+
+    public String getTestString() {
+        return testString;
+    }
+}
---
+++ src/test/java/AClassTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AClassTest {
+
+    @Test
+    public void testStringTest(){
+        AClass aClass = new AClass("test");
+        assertTrue("Equals strings.","test".equals(aClass.getTestString()));
+    }
+
+}
